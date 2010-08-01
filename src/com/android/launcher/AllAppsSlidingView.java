3b5bdefbff5a1c213b19e28b3447a5710d5ec03e
AnderWeb <anderweb@gmail.com>
2010-08-01 18:28:16 +0200

    Revert "ADW.Launcher is licensed under the GPLv3"
    Seems we cannot do this, so unless someone with proper knowledge (instead rough opinions)
    tell us how to preceed, we couldn't protect our work from closed source derivates :(
    
    This reverts commit 4b4a43b1d2da2a276179ebe748b6949b9a8caf98.

--- src/com/android/launcher/AllAppsSlidingView.java
+++ src/com/android/launcher/AllAppsSlidingView.java
@@ -1,22 +1,5 @@
-/*
-*    Copyright 2010 AnderWeb (Gustavo Claramunt) <anderweb@gmail.com>
-*
-*    This file is part of ADW.Launcher.
-*
-*    ADW.Launcher is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    ADW.Launcher is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with ADW.Launcher.  If not, see <http://www.gnu.org/licenses/>.
-*/
 package com.android.launcher;
+
 import com.android.launcher.HolderLayout.OnFadingListener;
 
 import android.content.Context;
