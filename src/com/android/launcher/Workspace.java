6b5132bd5fb424485086d8bcc5edb1bf08c6ec83
AnderWeb <anderweb@gmail.com>
2010-08-21 20:35:31 +0200

    sendWallpaperCommand could throw an exception cause of the window token being null

--- src/com/android/launcher/Workspace.java
+++ src/com/android/launcher/Workspace.java
@@ -1879,12 +1879,15 @@ public class Workspace extends WidgetSpace implements DropTarget, DragSource, Dr
 	 * @param hide
 	 */
     public void hideWallpaper(boolean hide) {
-    	if (hide)
-    		mWallpaperManager.sendWallpaperCommand(getWindowToken(),
-    				"hide", 0, 0, 0, null);
-    	else
-    		mWallpaperManager.sendWallpaperCommand(getWindowToken(),
-    				"show", 0, 0, 0, null);
+    	if(getWindowToken()!=null){
+	    	if (hide){
+	    		mWallpaperManager.sendWallpaperCommand(getWindowToken(),
+	    				"hide", 0, 0, 0, null);
+	    	}else{
+	    		mWallpaperManager.sendWallpaperCommand(getWindowToken(),
+	    				"show", 0, 0, 0, null);
+	    	}
+    	}
     }
     /**
      * ADW: Remove the specified screen and all the contents
