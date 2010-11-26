d89289bd7bb702dc4cd6e0d44d8d2082b5ff6647
AnderWeb <anderweb@gmail.com>
2010-11-26 12:06:45 +0100

    Fix desktop indicattor disappearing

--- src/com/android/launcher/DesktopIndicator.java
+++ src/com/android/launcher/DesktopIndicator.java
@@ -235,7 +235,7 @@ public class DesktopIndicator extends ViewGroup implements AnimationListener {
 	public void show() {
 		// TODO Auto-generated method stub
 		if(mVisibleTime<0){
-		    mIndicator.setVisibility(View.INVISIBLE);
+		    mIndicator.setVisibility(View.VISIBLE);
 			setVisibility(View.VISIBLE);
 		}
 	}
